299ff6d2d4c25478a990d71541c5a7025573a4e0
Saron Medeiros Philippi <saron.ph@gmail.com>
2021-07-06 21:26:02 -0300

    Add animation

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,11 +1,47 @@
 import React from 'react';
+import { Button, StyleSheet, Dimensions } from 'react-native';
+import Animated, { 
+  useSharedValue, 
+  useAnimatedStyle,
+  withTiming,
+  Easing
+} from 'react-native-reanimated';
 
 import * as S from './styles';
 
+const WIDTH = Dimensions.get('window').width;
+
 export default function Splash() {
+  const animation = useSharedValue(0);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: withTiming(animation.value, {
+          duration: 3000,
+          easing: Easing.bezier(.73,.17,0,1)
+        }) }
+      ]
+    }
+  })
+
+  function handleAnimationPosition() {
+    animation.value = Math.random()* (WIDTH - 100);
+  }
+
   return (
     <S.Container>
-      <S.Title>Splash</S.Title>
+      <Animated.View style={[styles.box, animatedStyles]} />
+
+      <Button title="Mover" onPress={handleAnimationPosition} />
     </S.Container>
   );
 }
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red'
+  }
+})
--- src/screens/Splash/styles.ts
+++ src/screens/Splash/styles.ts
@@ -3,11 +3,6 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
-  align-items: center;
 
   background-color: ${({ theme }) => theme.colors.header};
-`;
-
-export const Title = styled.Text`
-  font-size: 16px;
 `;
